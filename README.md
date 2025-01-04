9302cd997186e7adc381103d20b7532c064fb239
ks415 <git@stackblitz.com>
2025-01-05 03:33:04 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO-JS-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ks415/TODO-JS-)
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value;
+  alert(inputText);
+};
+
+document.getElementById('add-button').addEventListener('click', onClickAdd);

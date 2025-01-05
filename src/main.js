6fe9312ef769027fc5145b68223f8d22d1883a87
ks415 <kawauso415@gmail.com>
2025-01-06 01:50:04 +0900

    未完了リストに追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import './style.css';
 
 const onClickAdd = () => {
+  // テキストボックスの値を取得し，初期化する
   const inputText = document.getElementById('add-text').value;
-  alert(inputText);
+  document.getElementById('add-text').value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  })
+
+  //liタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  //未完成リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document.getElementById('add-button').addEventListener('click', onClickAdd);
